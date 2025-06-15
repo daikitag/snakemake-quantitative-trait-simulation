309ebe20520d4d8b65eb6c8d156094d2ef826a5e
daikitag <48062118+daikitag@users.noreply.github.com>
2025-06-15 10:49:17 +0100

    slurm-command

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --time 10:00:00
+#SBATCH --time 3-00:00
 
 #SBATCH --output="/vols/bitbucket/tagami/job_output/snakemake/%x-%A-std-output.out"
 #SBATCH --error="/vols/bitbucket/tagami/job_output/snakemake/%x-%A-err-output.out"
@@ -8,16 +8,17 @@
 #SBATCH --mail-user=daiki.tagami@hertford.ox.ac.uk
 #SBATCH --mail-type=ALL
 
-#SBATCH --mem=32G
+#SBATCH --mem=10G
 #SBATCH --cpus-per-task=1
 
 #SBATCH --cluster=swan
 #SBATCH --partition=standard-statgen-cpu
+#SBATCH --nodelist=smew01.cpu.stats.ox.ac.uk
 
 source /vols/bitbucket/tagami/python_environment/tskit_env/bin/activate
 
-cd /vols/bitbucket/tagami/snakemake-quantitative-trait-simulation
+cd /data/smew01/not-backed-up/tagami/snakemake-quantitative-trait-simulation
 
-snakemake --workflow-profile /vols/bitbucket/tagami/snakemake-quantitative-trait-simulation/profiles --use-conda
+snakemake --workflow-profile /data/smew01/not-backed-up/tagami/snakemake-quantitative-trait-simulation/profiles --use-conda
 
 deactivate
