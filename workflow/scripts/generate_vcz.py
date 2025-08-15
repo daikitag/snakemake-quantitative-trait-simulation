85f3937828a9adf99441abdf875dc5c67443715e
Daiki Tagami <daikitag@hotmail.co.jp>
2025-08-15 21:29:40 +0900

    Add VCZ conversion

--- workflow/scripts/generate_vcz.py
+++ workflow/scripts/generate_vcz.py
@@ -2,15 +2,56 @@ import tszip
 import numpy as np
 import pandas as pd
 
+import tskit
+import collections
+
+import bio2zarr.tskit as ts2z
+
 from snakemake.script import snakemake as snk
 
+
+def count_site_alleles(ts, tree, site):
+    counts = collections.Counter({site.ancestral_state: ts.num_samples})
+    for m in site.mutations:
+        current_state = site.ancestral_state
+        if m.parent != tskit.NULL:
+            current_state = ts.mutation(m.parent).derived_state
+        # Silent mutations do nothing
+        if current_state != m.derived_state:
+            num_samples = tree.num_samples(m.node)
+            counts[m.derived_state] += num_samples
+            counts[current_state] -= num_samples
+    return counts
+
+def maf_threshold(ts, maf):
+    remove_site = []
+    
+    tree = tskit.Tree(ts)
+    
+    for i in range(ts.num_sites):
+        site = ts.site(i)
+        tree.seek(site.position)
+        counts = count_site_alleles(ts, tree, site)
+        # counts is a Counter object from collections
+        max_allele_count = counts.most_common(1)[0][1]
+        freq = max_allele_count / ts.num_samples
+        if freq > (1 - maf):
+            remove_site.append(i)
+        # remove sites with 3+ alleles
+        elif len(counts) > 2:
+            remove_site.append(i)
+        # remove sites with 0 frequency
+        elif freq == 0:
+            remove_site.append(i)
+
+    down_sample_ts = ts.delete_sites(remove_site)
+    return down_sample_ts
 
 def update_result_dict(individual_ids, population, result_dict):
     for ind_id in individual_ids:
         result_dict["individual_id"].append(ind_id)
         result_dict["population"].append(population)
         result_dict["plink_id"].append(f"{population}_{ind_id}")
-
 
 def create_df(yri_individuals, ceu_individuals):
     result_dict = {
@@ -49,18 +90,25 @@ def obtain_individual_df(ts, yri_number, ceu_number, rng):
     
     return individual_id_df
 
-def main():    
-    ts = tszip.load(snk.input[0])
-    rng = np.random.default_rng(seed=int(snk.params.seed))
 
-    individual_id_df = obtain_individual_df(
-        ts = ts,
-        yri_number = int(snk.params.yri_number),
-        ceu_number = int(snk.params.ceu_number),
-        rng = rng
+def main():    
+    ts = tszip.load(snk.input.ts)
+    ts = maf_threshold(ts, maf=float(snk.params.maf))
+
+    individual_id_df = pd.read_csv(snk.input.individual_id)
+
+    model_mapping = ts.map_to_vcf_model(
+        individuals=individual_id_df["individual_id"],
+        individual_names=individual_id_df["plink_id"]
     )
-    
-    individual_id_df.to_csv(snk.output[0], index=False)
+
+    ts2z.convert(
+        ts,
+        vcz_path=snk.output[0],
+        model_mapping=model_mapping,
+        worker_processes=snk.threads
+    )
+
     
 if __name__ == '__main__':
     main()
