5f815d49725243bead9c191f5b61f316d57a35f0
Daiki Tagami <daikitag@hotmail.co.jp>
2025-08-16 18:09:21 +0900

    snakemake modification

--- workflow/scripts/generate_vcz.py
+++ workflow/scripts/generate_vcz.py
@@ -1,5 +1,4 @@
 import tszip
-import numpy as np
 import pandas as pd
 
 import tskit
@@ -7,7 +6,7 @@ import collections
 
 import bio2zarr.tskit as ts2z
 
-from snakemake.script import snakemake as snk
+#from snakemake.script import snakemake
 
 
 def count_site_alleles(ts, tree, site):
@@ -47,55 +46,12 @@ def maf_threshold(ts, maf):
     down_sample_ts = ts.delete_sites(remove_site)
     return down_sample_ts
 
-def update_result_dict(individual_ids, population, result_dict):
-    for ind_id in individual_ids:
-        result_dict["individual_id"].append(ind_id)
-        result_dict["population"].append(population)
-        result_dict["plink_id"].append(f"{population}_{ind_id}")
-
-def create_df(yri_individuals, ceu_individuals):
-    result_dict = {
-        "individual_id": [],
-        "plink_id": [],
-        "population": [],
-    }
-    update_result_dict(individual_ids=yri_individuals,
-                       population="YRI",
-                       result_dict=result_dict)
-    update_result_dict(individual_ids=ceu_individuals,
-                       population="CEU",
-                       result_dict=result_dict)
-    
-    return pd.DataFrame(result_dict)
-
-def obtain_population_id(ts):
-    for population in ts.populations():
-        if population.metadata["name"] == "EXPLOSIVE_YRI":
-            explosive_yri_id = population.id
-        elif population.metadata["name"] == "EXPLOSIVE_CEU":
-            explosive_ceu_id = population.id
-            
-    return explosive_yri_id, explosive_ceu_id
-
-def obtain_individual_df(ts, yri_number, ceu_number, rng):
-    explosive_yri_id, explosive_ceu_id = obtain_population_id(ts)
-    
-    yri_individuals = np.unique(ts.nodes_individual[ts.samples(explosive_yri_id)])
-    ceu_individuals = np.unique(ts.nodes_individual[ts.samples(explosive_ceu_id)])
-    
-    yri_selected_individuals = np.sort(rng.choice(yri_individuals, yri_number, replace=False))
-    ceu_selected_individuals = np.sort(rng.choice(ceu_individuals, ceu_number, replace=False))
-    
-    individual_id_df = create_df(yri_selected_individuals, ceu_selected_individuals)
-    
-    return individual_id_df
-
 
 def main():    
-    ts = tszip.load(snk.input.ts)
-    ts = maf_threshold(ts, maf=float(snk.params.maf))
+    ts = tszip.load(snakemake.input.ts)
+    ts = maf_threshold(ts, maf=float(snakemake.params.maf))
 
-    individual_id_df = pd.read_csv(snk.input.individual_id)
+    individual_id_df = pd.read_csv(snakemake.input.individual_id)
 
     model_mapping = ts.map_to_vcf_model(
         individuals=individual_id_df["individual_id"],
@@ -104,9 +60,9 @@ def main():
 
     ts2z.convert(
         ts,
-        vcz_path=snk.output[0],
+        vcz_path=snakemake.output[0],
         model_mapping=model_mapping,
-        worker_processes=snk.threads
+        worker_processes=snakemake.threads
     )
 
     
